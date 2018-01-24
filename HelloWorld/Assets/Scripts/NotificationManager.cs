7a71979c64b79c8f7163935a573b0b970cce482a
jayakrishna <jayakrishna.macharla@gmail.com>
2018-01-24 20:19:28 +0530

    merged the old branch

--- HelloWorld/Assets/Scripts/NotificationManager.cs
+++ HelloWorld/Assets/Scripts/NotificationManager.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour {
+public class NotificationManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Initialize ();
+		Initialize ();	
 	}
 
 	void Initialize()
 	{
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 }
